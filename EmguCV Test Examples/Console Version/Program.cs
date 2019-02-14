e8b55f14c44e1e98aad3d2de0b11ee47308c58bb
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2019-02-14 21:08:08 +0100

    initial

---
+++ EmguCV Test Examples/Console Version/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Emgu.CV;
+using Emgu.CV.UI;
+using Emgu.CV.Structure;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace EmguCV_Console_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImageViewer imageViewer = new ImageViewer();
+            VideoCapture videoCapture = new VideoCapture();
+
+            Application.Idle += new EventHandler(delegate (object sender, EventArgs e)
+            {
+                imageViewer.Image = videoCapture.QueryFrame();
+            });
+            imageViewer.ShowDialog();
+        }
+    }
+}
---
+++ EmguCV Test Examples/WPF/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using Emgu.CV;
+using Emgu.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+using System.Runtime.InteropServices;
+
+namespace EmguCV_WPF_Test
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+            dispatcherTimer.Tick += dispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 0, 0, 1);
+            dispatcherTimer.Start();
+
+            m_capture = new VideoCapture();
+            m_cascade = new CascadeClassifier("faceAlt2.xml");
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            using (Image<Bgr, byte> nextFrame = m_capture.QueryFrame().ToImage<Bgr, Byte>())
+            {
+                if (nextFrame != null)
+                {
+                    Image<Gray, byte> grayframe = nextFrame.Convert<Gray, byte>();
+
+                    var faces = m_cascade.DetectMultiScale(grayframe, 1.1, 3, new System.Drawing.Size(20, 20));
+
+                    foreach (var face in faces)
+                    {
+                        nextFrame.Draw(face, new Bgr(0, 0, 0), 3);
+                    }
+
+                    m_image.Source = ToBitmapSource(nextFrame);
+                }
+            }
+        }
+
+        private VideoCapture m_capture;
+        private CascadeClassifier m_cascade;
+
+        [DllImport("gdi32")]
+        private static extern int DeleteObject(IntPtr o);
+
+        public static BitmapSource ToBitmapSource(IImage image)
+        {
+            using (System.Drawing.Bitmap source = image.Bitmap)
+            {
+                IntPtr ptr = source.GetHbitmap(); //obtain the Hbitmap  
+
+                BitmapSource bs = System.Windows.Interop
+                  .Imaging.CreateBitmapSourceFromHBitmap(
+                  ptr,
+                  IntPtr.Zero,
+                  Int32Rect.Empty,
+                  System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+
+                DeleteObject(ptr); //release the HBitmap  
+                return bs;
+            }
+        }
+    }
+}
